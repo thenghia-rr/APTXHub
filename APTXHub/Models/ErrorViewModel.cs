e8d1b75d26d4c3a2f234ce6a7a4e065aa8a83b9f
thenghia <nghiadang085@gmail.com>
2025-06-18 18:45:53 +0700

    Add project files.

---
+++ APTXHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace APTXHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
