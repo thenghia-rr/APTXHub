484330aaa62596467b3e7dd5b9d9e5bed4961d9a
thenghia <nghiadang085@gmail.com>
2025-07-04 15:00:50 +0700

    feat(ui): add dark mode support basic

--- APTXHub/wwwroot/js/script.js
+++ APTXHub/wwwroot/js/script.js
@@ -1,18 +1,18 @@
 ﻿ // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('light') // Doi nguoc lai de TEST
-} else {
-    document.documentElement.classList.remove('dark')
-}
+//if (localStorage.theme === 'dark' || (!('theme' in localStorage))) {
+//    document.documentElement.classList.add('light') // Doi nguoc lai de TEST
+//} else {
+//    document.documentElement.classList.remove('dark')
+//}
 
-// Whenever the user explicitly chooses light mode
-localStorage.theme = 'light'
+//// Whenever the user explicitly chooses light mode
+//localStorage.theme = 'light'
 
-// Whenever the user explicitly chooses dark mode
-localStorage.theme = 'dark'
+//// Whenever the user explicitly chooses dark mode
+//localStorage.theme = 'dark'
 
-// Whenever the user explicitly chooses to respect the OS preference
-localStorage.removeItem('theme')
+//// Whenever the user explicitly chooses to respect the OS preference
+//localStorage.removeItem('theme')
 
 
 
@@ -54,3 +54,42 @@ localStorage.removeItem('theme')
 //    }
 //});
 
+
+
+// Set mode khi load trang
+if (localStorage.theme === 'dark') {
+    document.documentElement.classList.add('dark');
+} else {
+    document.documentElement.classList.remove('dark');
+}
+
+//function toggleDarkMode() {
+//    const html = document.documentElement;
+//    html.classList.toggle('dark');
+//    localStorage.theme = html.classList.contains('dark') ? 'dark' : 'light';
+
+//    updateThemeIcon();
+//}
+
+function toggleDarkMode() {
+    const html = document.documentElement;
+    const isDark = html.classList.contains('dark');
+
+    if (isDark) {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    } else {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    }
+
+    updateThemeIcon();
+}
+
+function updateThemeIcon() {    
+    const icon = document.getElementById('darkModeIcon');
+    if (!icon) return;
+
+    const isDark = document.documentElement.classList.contains('dark');
+    icon.setAttribute('name', isDark ? 'moon-outline' : 'sunny-outline');
+}
